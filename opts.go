be3f2aa1a9750c1dd0d1b72da272b6831acdf39a
Paweł Szczur <pawelszczur@gmail.com>
2019-09-27 09:42:51 +0200

    Add Google' default credentials.

--- opts.go
+++ opts.go
@@ -296,3 +296,35 @@ func GoogleLoggingAgent() Option {
 		return nil
 	}
 }
+
+// GoogleDefaultCredentials returns the token source for
+// "Application Default Credentials".
+//
+// It looks for credentials in the following places,
+// preferring the first location found:
+//
+//   1. A JSON file whose path is specified by the
+//      GOOGLE_APPLICATION_CREDENTIALS environment variable.
+//   2. A JSON file in a location known to the gcloud command-line tool.
+//      On Windows, this is %APPDATA%/gcloud/application_default_credentials.json.
+//      On other systems, $HOME/.config/gcloud/application_default_credentials.json.
+//   3. On Google App Engine standard first generation runtimes (<= Go 1.9) it uses
+//      the appengine.AccessToken function.
+//   4. On Google Compute Engine, Google App Engine standard second generation runtimes
+//      (>= Go 1.11), and Google App Engine flexible environment, it fetches
+//      credentials from the metadata server.
+func GoogleDefaultCredentials() Option {
+	return func(sh *StackdriverHook) error {
+		creds, err := google.FindDefaultCredentials(context.Background())
+		if err != nil {
+			return err
+		}
+
+		sh.projectID = creds.ProjectID
+		return HTTPClient(&http.Client{
+			Transport: &oauth2.Transport{
+				Source: creds.TokenSource,
+			},
+		})(sh)
+	}
+}
